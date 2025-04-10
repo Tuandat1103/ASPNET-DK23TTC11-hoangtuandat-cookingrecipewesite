b6dac9c62f0e18ad44466a2bc4f4492e0f0da97d
Tuandat113 <hoangthanhnguyen456@gmail.com>
2025-04-10 20:49:26 +0700

    Initial commit: Upload toàn bộ dự án Cooking Recipe Website

---
+++ CookingRecipeWebsite/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using CookingRecipeWebsite.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CookingRecipeWebsite.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên món ăn")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập nguyên liệu")]
+        public string Ingredients { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập hướng dẫn nấu")]
+        public string Instructions { get; set; }
+        public string ImagePath { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn danh mục")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; } // Cho phép NULL
+        public int UserId { get; set; }
+        public User? User { get; set; } // Cho phép NULL
+    }
+}
